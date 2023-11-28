7f11f6c5848980f8bb91a99bbc9968a3421da781
jonorgan <jonorgan26@gmail.com>
2023-11-28 16:06:11 -0500

    Finished question 2 of seeds

--- hw12_seeds.py
+++ hw12_seeds.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
 from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
 
 # Last digit of BUID: 6
 # R = 0, class L = 1 (negative) and L = 2 (positive)
@@ -23,28 +24,51 @@ print("Data subset split into training and testing data...")
 def SVM_clf(svm, svm_type):
 	svm.fit(X_train, Y_train.ravel())
 	svm_acc = svm.score(X_test, Y_test)
-	print("SVM " + svm_type + " accuracy: " + str(round(svm_acc * 100, 2)) + "%")
+	print(svm_type + " accuracy: " + str(round(svm_acc * 100, 2)) + "%")
 
 	predictions = svm.predict(X_test)
 	y_actu = pd.Series(Y_test.ravel(), name="Actual")
 	y_pred = pd.Series(predictions, name="Predicted")
 	cm = pd.crosstab(y_actu, y_pred)
-	print("SVM " + svm_type + " confusion matrix:")
+	print(svm_type + " confusion matrix:")
 	print(cm)
+	return [cm, svm_acc * 100]
 
-SVM_clf(svm.SVC(kernel='linear'), "Linear")
+cm_svmlin = SVM_clf(svm.SVC(kernel='linear'), "SVM Linear")
 print()
-SVM_clf(svm.SVC(kernel='rbf'), "Gaussian")
+cm_svmgauss = SVM_clf(svm.SVC(kernel='rbf'), "SVM Gaussian")
 print()
-SVM_clf(svm.SVC(kernel='poly', degree=3), "Polynomial")
+cm_svmpoly = SVM_clf(svm.SVC(kernel='poly', degree=3), "SVM Polynomial")
 
 
 print("\n")
 # Question 2 ========================================================================================
 print("Question 2:")
 
+cm_nb = SVM_clf(GaussianNB(), "Naive Bayesian")
+print()
+
+print("{:<16} {:<8} {:<8} {:<8} {:<8} {:<8} {:<8} {:<8}".format('Model' ,'TP', 'FP', 'TN', 'FN', 
+	'accuracy', 'TPR', 'TNR'))
+
+def PrintTableLine(cm, method, acc):
+	TP = cm[1].iloc[0]
+	FP = cm[1].iloc[1]
+	TN = cm[2].iloc[1]
+	FN = cm[2].iloc[0]
+	TPR = round(TP / (TP + FN), 2)
+	TNR = round(TN / (TN + FP), 2)
+	print("{:<16} {:<8} {:<8} {:<8} {:<8} {:<8} {:<8} {:<8}".format(method ,TP, FP, TN, FN, 
+		round(acc, 2), TPR, TNR))
 
 
+PrintTableLine(cm_svmlin[0], "SVM Linear", cm_svmlin[1])
+PrintTableLine(cm_svmgauss[0], "SVM Gaussian", cm_svmgauss[1])
+PrintTableLine(cm_svmpoly[0], "SVM Polynomial", cm_svmpoly[1])
+PrintTableLine(cm_nb[0], "Naive Bayesian", cm_nb[1])
 
+print("\n")
+# Question 3 ========================================================================================
+print("Question 3:")
 
 
