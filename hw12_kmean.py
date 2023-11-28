11cab34bd369edf8878393942b4885c61ff0aa94
jonorgan <jonorgan26@gmail.com>
2023-11-28 15:22:12 -0500

    Finished k-means assignment

--- hw12_kmean.py
+++ hw12_kmean.py
@@ -7,7 +7,6 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 df_cmg = pd.read_csv("cmg_weeks.csv")
-df_spy = pd.read_csv("spy_weeks.csv")
 
 # Question 1 ========================================================================================
 print("Question 1:")
@@ -62,10 +61,12 @@ while i < 50:
 print("{:<8} {:<8} {:<8}".format('Cluster', 'Green %', 'Red %'))
 
 count = 1
+cluster_percs = []
 for cluster in cluster_analysis:
 	total = cluster[0] + cluster[1]
 	green_perc = round((cluster[0] / total) * 100, 2)
 	red_perc = round((cluster[1] / total) * 100, 2)
+	cluster_percs.append([green_perc, red_perc])
 	print("{:<8} {:<8} {:<8}".format(str(count), str(green_perc) + "%", str(red_perc) + "%"))
 	count += 1
 
@@ -74,6 +75,13 @@ print("\n")
 # Question 3 ========================================================================================
 print("Question 3:")
 
+count = 1
+for cluster in cluster_percs:
+	if cluster[0] > 90:
+		print("Cluster " + str(count) + " is a pure green cluster (" + str(cluster[0]) + "%)")
+	elif cluster[1] > 90:
+		print("Cluster " + str(count) + " is a pure red cluster (" + str(cluster[1]) + "%)")
+	count += 1
 
 
 
